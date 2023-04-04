f6d05e2908f41b5fe2a1bf12aa99acaa99d67813
Vladislav Domorad <vdomorad@solvd.com>
2023-04-04 19:00:32 +0200

    Добавление redux persist

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 import "./index.css";
 import App from "./App";
-import store from "./redux/store";
+import store, { persistor } from "./redux/store";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -12,6 +13,8 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,7 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
+import {persistReducer, persistStore} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import themeReducer from "./reducers/themeSlice";
 import authReducer from "./reducers/authSlice";
@@ -8,17 +10,26 @@ import rootSaga from "./sagas/rootSaga";
 
 const sagaMiddleware = createSagaMiddleware();
 
+const rootReducer = combineReducers({
+  theme: themeReducer,
+  [postName]: postReduces,
+  auth: authReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = configureStore({
-  reducer: {
-    theme: themeReducer,
-    [postName]: postReduces,
-    auth: authReducer,
-  },
+  reducer: persistedReducer,
   middleware: [sagaMiddleware],
 });
 
 sagaMiddleware.run(rootSaga);
 
 export type RootState = ReturnType<typeof store.getState>;
+export let persistor = persistStore(store)
 
 export default store;
